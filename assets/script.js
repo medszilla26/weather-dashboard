7cdecccb76e66f4df1ac479e35ea50ef8cda400a
M3D5 <pablohdzjr@gmail.com>
2020-05-17 16:00:30 -0500

    successfully tested and ran api documentation from searchbox to city-div class

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,21 @@
-$("#find-city").on("click", function (event) {
-  event.preventDefault();
-
-  var city = $("#city-input").val();
+function searchWeatherAPI(city) {
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&APPID=2839b1208e73b48cb9463ede4e8db9f1";
-
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    $("#city-view").text(JSON.stringify(response));
+    console.log(response);
+    var cityName = $("<h3>").text(response.name);
+    $("#city-div").empty();
+    $("#city-div").append(cityName);
   });
+}
+
+$("#find-city").on("click", function (event) {
+  event.preventDefault();
+  var inputCity = $("#city-input").val().trim();
+  searchWeatherAPI(inputCity);
 });
