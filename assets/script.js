e3007da4fc183f620d84b444c872caef316c897d
M3D5 <pablohdzjr@gmail.com>
2020-05-17 13:19:15 -0500

    testing api, save point

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
-var URL = "http://api.openweathermap.org/data/2.5/";
-var apiKey = "APPID=2839b1208e73b48cb9463ede4e8db9f1";
+$("#find-city").on("click", function (event) {
+  event.preventDefault();
+
+  var city = $("#city-input").val();
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?" +
+    city +
+    "&APPID=2839b1208e73b48cb9463ede4e8db9f1";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    $("#city-view").text(JSON.stringify(response));
+  });
+});
